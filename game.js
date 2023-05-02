7f68777e2389bad13479f87aa3baca05669e0f13
vpk79 <someone_bul@yahoo.com>
2023-05-02 18:15:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,38 +1,35 @@
-function guessANumber(){
+function guessANumber() {
 
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-
-});
-
-let computerGuess = Math.floor(Math.random()*100);
-let guess;
-
-let recursiveAsyncReadLine = function () {
-    readline.question('Guess the number (0-100): ', number =>
-    {   guess = Number(number);
-        if (guess <= 100 && guess >= 0) {
-            if(guess == computerGuess){
-                console.log('You guess it!');
-                return readline.close();
-            } else if (guess > computerGuess){
-                console.log('Too high');
-                recursiveAsyncReadLine()
-            } else if (guess < computerGuess){
-                console.log('Too low');
-                recursiveAsyncReadLine()
-            }
-        
-        }
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
 
     });
-}
 
-recursiveAsyncReadLine();
+    let computerGuess = Math.floor(Math.random() * 100);
+    let guess;
 
+    let recursiveAsyncReadLine = function () {
+        readline.question('Guess the number (0-100): ', number => {
+            guess = Number(number);
+            if (guess <= 100 && guess >= 0) {
+                if (guess == computerGuess) {
+                    console.log('You guess it!');
+                    return readline.close();
+                } else if (guess > computerGuess) {
+                    console.log('Too high');
+                    recursiveAsyncReadLine()
+                } else if (guess < computerGuess) {
+                    console.log('Too low');
+                    recursiveAsyncReadLine()
+                }
 
+            }
+
+        });
+    }
+
+    recursiveAsyncReadLine();
 
 }
 
