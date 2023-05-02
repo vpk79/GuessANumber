606b32e9b0d0dec188a08a7d086094dabf211d84
vpk79 <someone_bul@yahoo.com>
2023-05-02 23:54:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -8,26 +8,42 @@ function guessANumber() {
 
     let computerGuess = Math.floor(Math.random() * 100);
     let guess;
+    let playerTrials = 6;
+
+
 
     let recursiveAsyncReadLine = function () {
-        readline.question('Guess the number (0-100): ', number => {
-            guess = Number(number);
-            if (guess <= 100 && guess >= 0) {
-                if (guess == computerGuess) {
-                    console.log('You guess it!');
-                    return readline.close();
-                } else if (guess > computerGuess) {
-                    console.log('Too high');
-                    recursiveAsyncReadLine()
-                } else if (guess < computerGuess) {
-                    console.log('Too low');
-                    recursiveAsyncReadLine()
+        if (playerTrials !== 0) {
+            console.log(`You have ${playerTrials} trials left`);
+            readline.question('Guess the number (0-100): ', number => {
+                guess = Number(number);
+
+                if (guess <= 100 && guess >= 0) {
+                    if (guess == computerGuess) {
+                        console.log('You guess it!');
+                        return readline.close();
+                    } else if (guess > computerGuess) {
+                        console.log('Too high!\n');
+                        playerTrials--;
+                        recursiveAsyncReadLine()
+                    } else if (guess < computerGuess) {
+                        console.log('Too low!\n');
+                        playerTrials--;
+                        recursiveAsyncReadLine()
+                    }
+
                 }
 
-            }
-
-        });
+            });
+        } else {
+            console.log('Sory! You Lose!');
+            return readline.close();
+        }
     }
+
+
+
+
 
     recursiveAsyncReadLine();
 
