5a8113c68efe47296e16f5eb12979ea77d79cd12
vpk79 <someone_bul@yahoo.com>
2023-05-03 19:40:00 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 function guessANumber() {
+    console.clear();
+    
 
     const readline = require('readline').createInterface({
         input: process.stdin,
@@ -6,44 +8,83 @@ function guessANumber() {
 
     });
 
-    let computerGuess = Math.floor(Math.random() * 100);
+
     let guess;
-    let playerTrials = 6;
-
-
+    let playerTrials = 10;
+    let level = 1;
+    let computerGuess = Math.floor(Math.random() * 100);
+    
+    let endGame = function () {
+       
+        readline.question("New Game? (Y/N) ", answer => {
+            if (answer === "Y" || answer === "y") {
+                console.clear();
+                readline.close();
+                guessANumber();
+            } else {
+                console.clear()
+                console.log("Have a nice day!");
+                return readline.close();
+            }
+        });
+    };
 
     let recursiveAsyncReadLine = function () {
+
+       
+        console.log("Press Ctrl+C for Exit.\n");
+       
+
         if (playerTrials !== 0) {
-            console.log(`You have ${playerTrials} trials left`);
-            readline.question('Guess the number (0-100): ', number => {
+            console.log(`Level ${level} - You will win if you beat level 10!\n`);
+            console.log(`You have ${playerTrials} trials left...`);
+
+            readline.question(`Guess the number (0-${level * 100}): `, number => {
                 guess = Number(number);
 
-                if (guess <= 100 && guess >= 0) {
+                if (guess <= (level * 100) && guess >= 0) {
                     if (guess == computerGuess) {
+                        console.clear();
                         console.log('You guess it!');
-                        return readline.close();
+                        level++;
+                            if (level > 10) {
+                                console.clear();
+                                console.log("Congratulations! You beat the game!");
+                                endGame();
+                                return;                        
+                            };
+
+                        playerTrials += 3 * level;
+                        console.log(`You gain ${3 * level} more trials. Good Luck!\n`);
+                        computerGuess = Math.floor(Math.random() * (100 * level));
+                        
+                        recursiveAsyncReadLine();
+                        // 
                     } else if (guess > computerGuess) {
-                        console.log('Too high!\n');
+                        console.clear()
+                        console.log("Too high\n");
                         playerTrials--;
                         recursiveAsyncReadLine()
                     } else if (guess < computerGuess) {
-                        console.log('Too low!\n');
+                        console.clear()
+                        console.log("Too low\n");
                         playerTrials--;
-                        recursiveAsyncReadLine()
+                        recursiveAsyncReadLine();
                     }
 
+                } else {
+                    console.clear()
+                    console.log("Invalid input! Try again...");
+                    recursiveAsyncReadLine();
                 }
 
             });
         } else {
-            console.log('Sory! You Lose!');
-            return readline.close();
+            console.log('Sorry! You Lose!\n');
+            endGame();
+            // return readline.close();
         }
     }
-
-
-
-
 
     recursiveAsyncReadLine();
 
